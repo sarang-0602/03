cc25eb3296bf870d904fbec5f7ef0035f9ac179a
sarang-0602 <lovesarang060225@gmail.com>
2026-09-17 16:29:57 +0900

    test4

--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 
 int main(void)
 {
-    char c;
-    char next;
+    double a, b;
+    double result;
 
-    printf("enter a character : ");
-    scanf("%c", &c);
+    printf("분자를 입력하세요 : ");
+    scanf("%lf", &a);
 
-    next = c + 1;
+    printf("분모를 입력하세요 : ");
+    scanf("%lf", &b);
 
-    printf("the next character of %c (%i) is %c (%i)\n", c, c, next, next);
+    result = a / b; 
+
+    printf("나누기의 결과는 %f 입니다.\n", result);
 
     return 0;
 }
